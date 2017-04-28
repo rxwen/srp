488ef47f67e2e1404244e4a8f6b91498466e5fb7
rxwen <rx.wen218@gmail.com>
2017-04-28 19:10:57 +0800

    FC2945 key len align with sha digest size

--- libsrp/srp.h
+++ libsrp/srp.h
@@ -321,8 +321,8 @@ _TYPE( SRP_RESULT ) SRP_respond P((SRP * srp, cstr ** response));
 
 /* RFC2945-style SRP authentication */
 
-#define RFC2945_KEY_LEN 40	/* length of session key (bytes) */
-#define RFC2945_RESP_LEN 20	/* length of proof hashes (bytes) */
+#define RFC2945_KEY_LEN SHA_DIGESTSIZE	/* length of session key (bytes) */
+#define RFC2945_RESP_LEN SHA_DIGESTSIZE	/* length of proof hashes (bytes) */
 
 /*
  * RFC2945-style SRP authentication methods.  Use these like:
